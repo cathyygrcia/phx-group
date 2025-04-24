9c51af793dd7f9fda55c0cf71b0f281c94293fd3
Cathy Garcia <garciacathy12@gmail.com>
2025-04-24 14:25:56 -0700

    Successfully installed react icons and cleaned up the mobile version

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,19 +1,20 @@
+import { GiHamburgerMenu } from "react-icons/gi";
+
 export default function Navbar() {
   return (
-    <>
-      <nav className="flex justify-around bg-red-700 p-8 w-screen">
-        <div className="flex basis-2/4	justify-center">
-          <div>
-            <img src="/images/logo.png" alt="logo" />
-          </div>
-        </div>
-        <ul className="flex items-center justify-end lg:text-white basis-2/4">
-          <li className="lg:pr-16 ">Home</li>
-          <li className="lg:pr-16">Services</li>
-          <li className="lg:pr-16">About</li>
-          <li className="lg:pr-16">Contact</li>
-        </ul>
-      </nav>
-    </>
+    <nav className="flex justify-around bg-red-700 p-8 w-screen">
+      <div className="flex basis-2/4 justify-center">
+        <img src="/images/logo.png" alt="logo" />
+      </div>
+      <div className="flex items-center text-white text-3xl">
+        <GiHamburgerMenu className="sm:block md:hidden" />
+      </div>
+      <ul className="hidden md:flex items-center justify-end md:text-white basis-2/4">
+        <li className="md:pr-8 lg:pr-16">Home</li>
+        <li className="md:pr-8 lg:pr-16">Services</li>
+        <li className="md:pr-8 lg:pr-16">About</li>
+        <li className="md:pr-8 lg:pr-16">Contact</li>
+      </ul>
+    </nav>
   );
 }
