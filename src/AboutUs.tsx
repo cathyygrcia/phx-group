ebe7ced857b11d9ba5949fda556444493ca89771
cathyygrcia <garciacathy12@gmail.com>
2025-05-08 13:31:34 -0700

    cleaned up mobile version

--- src/AboutUs.tsx
+++ src/AboutUs.tsx
@@ -1,38 +1,39 @@
 export default function AboutUs() {
-  return (
-    <div>
-      <div className="flex flex-col items-center bg-red-900 p-2 sm:p-16 md:p-20">
-        <h1 className="flex justify-center pb-8 text-white text-5xl">
-          We Are Phoenix Group
-        </h1>
-        <hr className="w-1/4 border-t border-gray-300 mb-12" />
-        <div className="w-3/4">
-          <h2 className="text-white text-center text-2xl leading-relaxed">
-            Founded in 1988. Phoenix Group represents a unique blend of
-            experience and expertise unmatched in the parking and citation
-            management industry. Our reputation as an industry leader is
-            evidence of our strength in customer loyalty and proven customer
-            satisfaction. Phoenix Group realizes that our most important asset
-            is our ability to identify and fulfill the needs of our customers.
-            Phoenix Group exceeds expectations while committing to quality,
-            efficiency, and responsiveness; all while growing with our
-            customers. This common sense approach is what distinguishes Phoenix
-            Group from the rest.
-          </h2>
-          <h2 className="text-white text-center text-2xl leading-relaxed mt-7">
-            As The industry leader in cutting-edge mobile handheld solutions for
-            cities, municipalities, and schools, Phoenix Group takes great pride
-            in a top-quality product line and our highly recognized service
-            solutions. The Phoenix Group Mobile Android solution offers you the
-            ability to utilize our state-of-the-art Wincite application on a
-            variety of Android compatible hardware. Phoenix Group manages every
-            phase in the life of any citation. We will facilitate the violation
-            process through its entire life cycle: Processing the citation,
-            notice of violation, online services, collections, FTB, and
-            hearings.
-          </h2>
+    return (
+      <div>
+        <div className="flex flex-col items-center bg-red-900 p-8 sm:p-16 md:p-20">
+          <h1 className="text-white text-3xl sm:text-4xl md:text-5xl text-center pb-8">
+            We Are Phoenix Group
+          </h1>
+          <hr className="w-1/4 border-t border-gray-300 mb-12" />
+          <div className="w-3/4">
+            <h2 className="text-white text-lg sm:text-xl md:text-2xl leading-relaxed text-center">
+              Founded in 1988, Phoenix Group represents a unique blend of
+              experience and expertise unmatched in the parking and citation
+              management industry. Our reputation as an industry leader is
+              evidence of our strength in customer loyalty and proven customer
+              satisfaction. Phoenix Group realizes that our most important asset
+              is our ability to identify and fulfill the needs of our customers.
+              Phoenix Group exceeds expectations while committing to quality,
+              efficiency, and responsiveness — all while growing with our
+              customers. This common sense approach is what distinguishes Phoenix
+              Group from the rest.
+            </h2>
+            <h2 className="text-white text-lg sm:text-xl md:text-2xl leading-relaxed text-center mt-7">
+              As the industry leader in cutting-edge mobile handheld solutions for
+              cities, municipalities, and schools, Phoenix Group takes great pride
+              in a top-quality product line and our highly recognized service
+              solutions. The Phoenix Group Mobile Android solution offers you the
+              ability to utilize our state-of-the-art Wincite application on a
+              variety of Android-compatible hardware. Phoenix Group manages every
+              phase in the life of any citation. We facilitate the violation
+              process through its entire life cycle: processing the citation,
+              notice of violation, online services, collections, FTB, and
+              hearings.
+            </h2>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+  
