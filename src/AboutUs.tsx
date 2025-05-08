47f2026b0980bcba0ae3578d26625eea3f036542
cathyygrcia <garciacathy12@gmail.com>
2025-05-08 12:36:09 -0700

    Working on the wording for the about us section

--- src/AboutUs.tsx
+++ src/AboutUs.tsx
@@ -1,16 +1,38 @@
 export default function AboutUs() {
   return (
     <div>
-      <div className="h-screen flex flex-col items-center  bg-red-900">
-        <h1 className="  flex justify-center pt-10 pb-16 text-white text-3xl">
-          About Us
-        </h1>
-        <h2 className="text-white text-center">
-          Founded in 1988. Phoenix Group represents a unique blend of experience
-          and expertise unmatched in the parking and citation management
-          industry. Our reputation as an industry leader is evidence of our
-          strength in customer loyalty and proven customer satisfaction.
-        </h2>
+    <div className="flex flex-col items-center bg-red-900">
+  <h1 className="flex justify-center pt-10 pb-4 text-white text-5xl">
+    We Are Phoenix Group
+  </h1>
+  <hr className="w-1/4 border-t border-gray-300 mb-12" />
+
+        <div className="w-3/4">
+          <h2 className="text-white text-center text-2xl leading-relaxed">
+            Founded in 1988. Phoenix Group represents a unique blend of
+            experience and expertise unmatched in the parking and citation
+            management industry. Our reputation as an industry leader is
+            evidence of our strength in customer loyalty and proven customer
+            satisfaction. Phoenix Group realizes that our most important asset
+            is our ability to identify and fulfill the needs of our customers.
+            Phoenix Group exceeds expectations while committing to quality,
+            efficiency, and responsiveness; all while growing with our
+            customers. This common sense approach is what distinguishes Phoenix
+            Group from the rest.
+          </h2>
+          <h2 className="text-white text-center text-2xl leading-relaxed mt-7">
+            As The industry leader in cutting-edge mobile handheld solutions for
+            cities, municipalities, and schools, Phoenix Group takes great pride
+            in a top-quality product line and our highly recognized service
+            solutions. The Phoenix Group Mobile Android solution offers you the
+            ability to utilize our state-of-the-art Wincite application on a
+            variety of Android compatible hardware. Phoenix Group manages every
+            phase in the life of any citation. We will facilitate the violation
+            process through its entire life cycle: Processing the citation,
+            notice of violation, online services, collections, FTB, and
+            hearings.
+          </h2>
+        </div>
       </div>
     </div>
   );
